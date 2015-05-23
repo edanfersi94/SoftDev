556133064e6da76cdd9dcf00d1f13d533b4c4c04
oriana <oggs_22@hotmail.com>
2015-05-22 23:04:46 -0430

    Se modifico prod.py 			vproducto.html 			Habria que arreglar indentacion de los nombres de la columna
    Tambien hay errores con el test 18 de acciones que no se elimina.Mismo
    caso con test21 de objetivo

--- app/scrum/prod.py
+++ app/scrum/prod.py
@@ -72,7 +72,7 @@ def VProducto():
     pilas = [{'idPila':1, 'nombre':'Pagos en línea', 'descripcion':'Pagos usando tarjeta de débito'}]
     res['fPila'] = pilas[idPila-1]
     res['data3'] = [
-        {'idActor':act.id_actores, 'descripcion':act.nombre_actores}
+        {'idActor':act.id_actores, 'nombre':act.nombre_actores,'descripcion':act.descripcion_actores}
         for act in actores]
     res['data5'] = [
         {'idAccion':acc.idacciones, 'descripcion':acc.descripAcciones}
