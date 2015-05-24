6ea7cb24b3230e62b0784835a5ddd9e40cb8558d
oriana <oggs_22@hotmail.com>
2015-05-24 16:22:36 -0430

    Archivos complementarios

--- app/scrum/actor.py
+++ app/scrum/actor.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+
+# Librerias a importar.
 from flask import request, session, Blueprint, json
+from app.scrum.funcActor import clsActor
+import model
 
 actor = Blueprint('actor', __name__)
 
+#.----------------------------------------------------------------------------------------.
 
 @actor.route('/actor/ACrearActor', methods=['POST'])
 def ACrearActor():
@@ -10,12 +15,21 @@ def ACrearActor():
     params = request.get_json()
     results = [{'label':'/VProducto', 'msg':['Actor creado']}, {'label':'/VCrearActor', 'msg':['Error al crear actor']}, ]
     res = results[0]
-    #Action code goes here, res should be a list with a label and a message
+
+    nuevo_nombre_actores      = params['nombre']
+    nueva_descripcion_actores = params['descripcion']
+    
+    nuevoActor   = clsActor()
+    resultInsert = nuevoActor.insert_Actor(nuevo_nombre_actores, nueva_descripcion_actores)
+
+    if ( resultInsert ):
+        res = results[0]
+    else:
+        res = results[1]
 
     idPila = 1
     res['label'] = res['label'] + '/' + str(idPila)
 
-    #Action code ends here
     if "actor" in res:
         if res['actor'] is None:
             session.pop("actor", None)
@@ -23,7 +37,7 @@ def ACrearActor():
             session['actor'] = res['actor']
     return json.dumps(res)
 
-
+#.----------------------------------------------------------------------------------------.
 
 @actor.route('/actor/AModifActor', methods=['POST'])
 def AModifActor():
@@ -31,12 +45,25 @@ def AModifActor():
     params = request.get_json()
     results = [{'label':'/VProducto', 'msg':['Actor actualizado']}, {'label':'/VActor', 'msg':['Error al modificar actor']}, ]
     res = results[0]
-    #Action code goes here, res should be a list with a label and a message
 
     idPila = 1
     res['label'] = res['label'] + '/' + str(idPila)
 
-    #Action code ends here
+    productoActual = model.EstadoActual.id_producto_actual == idPila
+    query = model.db.session.query(model.EstadoActual).filter(productoActual).all()
+    
+    id_actor = query[0].id_actor_actual
+    nuevo_nombre_actores = params['nombre']
+    nueva_descripcion_actores = params['descripcion']
+
+    actorModif = clsActor()
+    resultsModif = actorModif.modify_Actor(id_actor, nuevo_nombre_actores, nueva_descripcion_actores)
+
+    if ( resultsModif ):
+        res = results[0]
+    else:
+        res = results[1]
+
     if "actor" in res:
         if res['actor'] is None:
             session.pop("actor", None)
@@ -44,38 +71,34 @@ def AModifActor():
             session['actor'] = res['actor']
     return json.dumps(res)
 
-
+#.----------------------------------------------------------------------------------------.
 
 @actor.route('/actor/VActor')
 def VActor():
     res = {}
     if "actor" in session:
         res['actor']=session['actor']
-    #Action code goes here, res should be a JSON structure
 
     res['idPila'] = 1 
 
-    #Action code ends here
+    pagActorActual= request.url
+    pagActorActual.split('=')
+    actorActual = int(pagActorActual[-1])
+
+    productoActual = model.EstadoActual.id_producto_actual == 1
+    model.db.session.query(model.EstadoActual).filter(productoActual).\
+        update({'id_actor_actual':actorActual})
+    model.db.session.commit()    
+
     return json.dumps(res)
 
-
+#.----------------------------------------------------------------------------------------.
 
 @actor.route('/actor/VCrearActor')
 def VCrearActor():
     res = {}
     if "actor" in session:
         res['actor']=session['actor']
-    #Action code goes here, res should be a JSON structure
-
-
-    #Action code ends here
     return json.dumps(res)
 
-
-
-
-
-#Use case code starts here
-
-
-#Use case code ends here
+#.----------------------------------------------------------------------------------------.
