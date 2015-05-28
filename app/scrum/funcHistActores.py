fc36677608c5c2078a62f37b4780c167ea0b62fd
Edward Fernandez <edwardfernandez26@gmail.com>
2015-05-28 19:23:29 -0430

    Módulo que permite verificar insertar en las historias.

---
+++ app/scrum/funcHistActores.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Función a importar.
+import model
+
+# Clase que tendra las diferentes funcionalidades de la tabla " ActoresHistorias "
+class clsHistoriaActores():
+
+	#-------------------------------------------------------------------------------
+
+	def insert_Actor(self, idHistoria, idActor):
+
+		# Búsqueda del identificador más alto.
+		query = model.db.session.query(model.func.max(model.ActoresHistorias.idHistoria)).all()
+
+		# Se toma la tupla resultante.
+		tuplaResult = query[0]
+
+		num_actoresInsertado = tuplaResult[0]
+
+		# Booleanos que indican si el tipo es el correcto.
+		idHistoriaIsInt = type(idHistoria) == int
+		idActoresIsInt = type(idActor) == int
+
+		if (idHistoriaIsInt and idActoresIsInt):
+
+			# Booleanos que indican si se cumplen los límites.
+			idHistoriaIsPos = idHistoria > 0
+			idActoresIsPos = idActor > 0
+
+			if (idHistoriaIsPos and idActoresIsPos):
+
+				# Si no hay acciones en la base de datos, entonces se inicializa 
+				# el contador.
+				if (num_actoresInsertado == None):
+					num_actoresInsertado = 0
+				num_actoresInsertado = num_actoresInsertado + 1
+
+				newActor = model.ActoresHistorias(num_actoresInsertado, idHistoria, idActor)
+				model.db.session.add(newActor)
+				model.db.session.commit()
+				return( True )
+
+		return( False )
+
+		#-------------------------------------------------------------------------------
+
---
+++ app/scrum/funcHistObjetivo.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Función a importar.
+import model
+
+# Clase que tendra las diferentes funcionalidades de la tabla " ObjHistorias "
+class clsHistoriaObj():
+
+	#-------------------------------------------------------------------------------
+
+	def insert_Objetivo(self, idHistoria, idObjetivo):
+
+		# Búsqueda del identificador más alto.
+		query = model.db.session.query(model.func.max(model.ObjHistorias.idHistoria)).all()
+
+		# Se toma la tupla resultante.
+		tuplaResult = query[0]
+
+		num_objInsertado = tuplaResult[0]
+
+		# Booleanos que indican si el tipo es el correcto.
+		idHistoriaIsInt = type(idHistoria) == int
+		idObjetivoIsInt = type(idObjetivo) == int
+
+		if (idHistoriaIsInt and idObjetivoIsInt):
+
+			# Booleanos que indican si se cumplen los límites.
+			idHistoriaIsPos = idHistoria > 0
+			idObjetivoIsPos = idObjetivo > 0
+
+			if (idHistoriaIsPos and idObjetivoIsPos):
+
+				# Si no hay acciones en la base de datos, entonces se inicializa 
+				# el contador.
+				if (num_objInsertado == None):
+					num_objInsertado = 0
+				num_objInsertado = num_objInsertado + 1
+
+				newObj = model.ObjHistorias(num_objInsertado ,idHistoria, idObjetivo)
+				model.db.session.add(newObj)
+				model.db.session.commit()
+				return( True )
+
+		return( False )
+
+		#-------------------------------------------------------------------------------
+
