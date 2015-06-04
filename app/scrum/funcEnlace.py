9d774ce26bc5267d9ef61538ed4848b71c4b84de
oriana <oggs_22@hotmail.com>
2015-06-03 23:06:32 -0430

    Casos de prueba de insertar Enlace

--- app/scrum/funcEnlace.py
+++ app/scrum/funcEnlace.py
@@ -7,53 +7,69 @@ class clsEnlace():
 		queryNumEnlaces = model.db.session.query(model.func.max(model.Enlaces.id_enlace)).all()
 		queryNumHistorias = model.db.session.query(model.func.max(model.Historia_Usuario.idHistoria_Usuario)).all()
 
-		tuplaResult = queryNumHistorias[0]
-		idHistoria = tuplaResult[0]
+		idProductoIsInt = type(idProductoActual) == int
+		idSuperIsInt = type(idSuper) == int  
+		
+		
+		if (idProductoIsInt and idSuperIsInt):
+			
+			producto = model.db.session.query(model.Pila.idPila).filter(model.Pila.idPila == idProductoActual).all()
 
-		if (idHistoria == None):
-			idHistoria = 0
-		idHistoria += 1
+			idProducto= [int(i[0]) for i in producto]
 
-		tuplaResult = queryNumEnlaces[0]
-		num_enlaces = tuplaResult[0]
-
-		if (num_enlaces == None):
-			num_enlaces = 0
-		num_enlaces += 1
-
-		listaEnlace = {}
-		salida = False
-		# Se genera la lista.
-		for elem in query:
-			if (elem.id_clave in listaEnlace):
-				listaEnlace[elem.id_clave] += [elem.id_valor]
-			else:
-				listaEnlace[elem.id_clave] = [elem.id_valor]
-		print(listaEnlace)
-
-		target = {}
-		for key in listaEnlace:
-			print(key)
-			if key == idSuper:
-				target[key] = listaEnlace[key] + [idHistoria]
-			else:
-				target[key] = listaEnlace[key]
-
-		if not(idSuper in target):
-			target[idSuper] = [idHistoria]
-
-		print(idHistoria)
-		target[idHistoria] = []
-		print(target)
-
-		existCiclo = self.existenciaCiclo(target)
-
-		if not(existCiclo):
-			newEnlace = model.Enlaces(num_enlaces, idProductoActual, idSuper, idHistoria)
-			model.db.session.add(newEnlace)
-			model.db.session.commit()
-			salida = True
-		return(salida)
+			idProductoIsEsta = idProductoActual in idProducto
+			
+			if (idProductoIsEsta):
+		
+				tuplaResult = queryNumHistorias[0]
+				idHistoria = tuplaResult[0]
+		
+				if (idHistoria == None):
+					idHistoria = 0
+				idHistoria += 1
+		
+				tuplaResult = queryNumEnlaces[0]
+				num_enlaces = tuplaResult[0]
+		
+				if (num_enlaces == None):
+					num_enlaces = 0
+				num_enlaces += 1
+		
+				listaEnlace = {}
+				salida = False
+				# Se genera la lista.
+				for elem in query:
+					if (elem.id_clave in listaEnlace):
+						listaEnlace[elem.id_clave] += [elem.id_valor]
+					else:
+						listaEnlace[elem.id_clave] = [elem.id_valor]
+				print(listaEnlace)
+		
+				target = {}
+				for key in listaEnlace:
+					print(key)
+					if key == idSuper:
+						target[key] = listaEnlace[key] + [idHistoria]
+					else:
+						target[key] = listaEnlace[key]
+		
+				if not(idSuper in target):
+					target[idSuper] = [idHistoria]
+		
+				print(idHistoria)
+				target[idHistoria] = []
+				print(target)
+		
+				existCiclo = self.existenciaCiclo(target)
+		
+				if not(existCiclo):
+					newEnlace = model.Enlaces(num_enlaces, idProductoActual, idSuper, idHistoria)
+					model.db.session.add(newEnlace)
+					model.db.session.commit()
+					salida = True
+				return(salida)
+		else:
+			return(False)
 
    #-------------------------------------------------------------------------------
 
---
+++ app/scrum/testing/testEnlace.py
@@ -0,0 +1,295 @@
+"""
+    UNIVERSIDAD SIMON BOLIVAR
+    Departamento de Computacion y Tecnologia de la Informacion.
+    CI-3715 - Ingenieria de Software I (CI-3715)
+    Abril - Julio 2015
+    AUTORES:
+        
+    DESCRIPCION: Script que contiene los casos de prueba del modulo "funcEnlace.py"
+    
+"""
+
+#------------------------------------------------------------------------------------
+
+# Librerias a utilizar.
+import os
+import sys
+from sqlalchemy.ext.baked import Result
+
+# PATH que permite utilizar al modulo "model.py"
+sys.path.append('../../../')
+import model
+
+sys.path.append('../')
+from funcProducto import clsProducto
+from funcEnlace import clsEnlace
+
+import unittest
+
+class TestEnlace(unittest.TestCase):
+    
+    def vaciarBaseDeDatos(self):
+        model.db.session.query(model.Enlaces).delete()
+        model.db.session.query(model.Pila).delete()
+    
+    def InsertarProducto(self):
+        #Datos a ingresar a la tabla de pila
+        NewIdPila = 1
+        NewdescripProducto = "PruebaPila1"
+        
+        #Se ingresa manualmente los datos a la tabla pila
+        newPila = model.Pila(NewIdPila, NewdescripProducto)
+        model.db.session.add(newPila)
+        model.db.session.commit()
+        
+    #Se prueba que exista la clase clsEnlace
+    def testEnlaceExist(self):
+        model.db.session.query(model.Pila).delete()
+        model.db.session.query( model.Enlaces ).delete() # Se limpia la base de datos
+        tempEnlace = clsEnlace()
+        self.assertIsNotNone(tempEnlace)
+        
+    #-------------------------------------------------------------------------------------
+    #FUNCION INSERTAR
+    
+    #CASO VALIDO
+    #test ; Se inserta idProducto y IdSuper de tipo int
+    
+    def testEnlaceValid(self):
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idEnlace = 1
+        idproducto = 1
+        idclave = 1
+        idvalor= 1
+        
+        newEnlace = model.Enlaces(idEnlace,idproducto,idclave,idvalor)
+        model.db.session.add(newEnlace)
+        model.db.session.commit()
+    
+    # test : Se inserta un idProducto y idSuper correctos
+    def testinsertarValid(self):
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = 1
+        idproducto = 1
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertTrue(result)
+    
+    # test Se inserta un idProdicto de tipo None y idSuper de tipo int
+    def testinsertIdProductoNone(self):
+        
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = 1
+        idproducto = None
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertFalse(result)
+        
+    # test Se inserta un idProdicto de tipo String y idSuper de tipo int    
+    def testinsertIdProductoStr(self):
+        
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = 1
+        idproducto = "HOLA"
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertFalse(result)
+        
+    # test Se inserta un idProdicto vacio y idSuper de tipo int    
+    def testinsertIdProductoEmpty(self):
+        
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = 1
+        idproducto = ""
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertFalse(result)
+    
+    # test Se inserta un idProdicto de tipo Lista y idSuper de tipo int    
+    def testinsertIdProductoList(self):
+        
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = 1
+        idproducto = []
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertFalse(result)
+    
+    # test Se inserta un idProdicto de tipo float y idSuper de tipo int    
+    def testinsertIdProductoFLoat(self):
+        
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = 1
+        idproducto = 2.5656565
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertFalse(result)
+        
+    # test Se inserta un idProdicto de tipo numero entero negativo y idSuper de tipo int    
+    def testinsertIdProductoNegNum(self):
+        
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = 1
+        idproducto =-34.0
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertFalse(result)
+    
+    # test Se inserta un idProdicto de tipo int y idSuper de tipo None  
+    def testinsertIdSuperNone(self):
+        
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = None
+        idproducto = 1
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertFalse(result)
+        
+    # test Se inserta un idProducto de tipo int y idSuper de tipo String  
+    def testinsertIdSuperString(self):
+        
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = None
+        idproducto = 1
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertFalse(result)
+        
+    # test Se inserta un idProducto de tipo int y idSuper vacio 
+    def testinsertIdSuperEmpty(self):
+        
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = ""
+        idproducto = 1
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertFalse(result)
+        
+    # test Se inserta un idProducto de tipo int y idSuper de tipo List 
+    def testinsertIdSuperList(self):
+        
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = []
+        idproducto = 1
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertFalse(result)
+        
+    # test Se inserta un idProducto de tipo int y idSuper de tipo Float 
+    def testinsertIdSuperFLoat(self):
+        
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = 2.454544
+        idproducto = 1
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertFalse(result)
+        
+    # test Se inserta un idProducto de tipo int y idSuper de tipo Numeros Negativos 
+    def testinsertIdSuperNegNum(self):
+        
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = -2333.1
+        idproducto = 1
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertFalse(result)
+        
+    #test: El idProducto no se encuentra en la tabla Pila
+    def testinsertIdProductNoExist(self):
+        
+        #Se borra la base de datos
+        self.vaciarBaseDeDatos()
+        
+        #Se introduce datos a la tabla producto
+        self.InsertarProducto()
+        
+        idSuper = 1
+        idproducto = 345446
+        tempEnlace = clsEnlace()
+        
+        result = tempEnlace.insert_Enlace(idproducto,idSuper)
+        self.assertFalse(result)
