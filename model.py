0ed50b46152ac3bb66e247ad60c9c6afa292d35a
oriana <oggs_22@hotmail.com>
2015-06-04 11:51:49 -0430

    Añadi campo super a la tabla historia

--- model.py
+++ model.py
@@ -72,15 +72,17 @@ class Historia_Usuario(db.Model):
     codigoHistoria_Usuario   = db.Column(db.String(10), primary_key=True,)
     id_Pila_Historia_Usuario = db.Column(db.Integer, db.ForeignKey('pila.idPila'))
     id_Acciones_Historia_Usuario = db.Column(db.Integer, db.ForeignKey('acciones.idacciones'))
+    idSuper                  = db.Column(db.Integer, nullable=True)
     listaObjetivos = db.relationship('ObjHistorias',backref='historia',cascade = "all, delete, delete-orphan")
     listaActores = db.relationship('ActoresHistorias',backref='historia',cascade = "all, delete, delete-orphan")
 
-    def __init__(self, idHistoria,codigoHistoria,historiaIdPila, tipoHistoria_Usuario,id_Acciones_Historia_Usuario):
+    def __init__(self, idHistoria,codigoHistoria,historiaIdPila, tipoHistoria_Usuario,id_Acciones_Historia_Usuario,idSuper):
         self.idHistoria_Usuario  = idHistoria
         self.tipoHistoria_Usuario = tipoHistoria_Usuario
         self.codigoHistoria_Usuario = codigoHistoria
         self.id_Pila_Historia_Usuario = historiaIdPila
         self.id_Acciones_Historia_Usuario = id_Acciones_Historia_Usuario
+        self.idSuper = idSuper
 
 # Tabla Usuario.
 class User(db.Model):
