81e552cda3f7944f93911dd09f96df4f9c9010e7
Edward Fernandez <edwardfernandez26@gmail.com>
2015-06-04 18:07:40 -0430

    Se añadió la linea de prioridad.

--- app/scrum/historias.py
+++ app/scrum/historias.py
@@ -116,6 +116,7 @@ def AModifHistoria():
     objetivosAsociar = params.get('objetivos', None)
     actoresAsociar = params.get('actores', None)
     superAsociar = params.get('super', None)
+    prioridadAsociar = params.get('prioridad', None)
 
     #print("tipoAsociar",tipoAsociar,"codigo",codigoHistoria,"accion",accionAsociar,"obj",objetivosAsociar,"act",actoresAsociar,"super",superAsociar)
 
