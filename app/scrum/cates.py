193f4dd7b6fc7b3808eb63e6330207b155ac5b6c
Edward Fernandez <edwardfernandez26@gmail.com>
2015-06-22 17:18:25 -0430

    Se acomodaron las vistas.

--- app/scrum/cates.py
+++ app/scrum/cates.py
@@ -22,25 +22,28 @@ from model import db,Categorias
 
 cates = Blueprint('cates', __name__)
 
+#.-----------------------------------------------------------------------------.
 
 @cates.route('/cates/ACrearCategoria', methods=['POST'])
 def ACrearCategoria():
-    #POST/PUT parameters
+
+    if 'usuario' not in session:
+      res['logout'] = '/'
+      return json.dumps(res)
+
     params = request.get_json()
-    results = [{'label':'/VCategorias', 'msg':['Categoría creada.']}, {'label':'/VCategorias', 'msg':['Error al intentar crear categoría.']}, ]
+    results = [{'label':'/VCategorias', 'msg':['Categoría creada.']}, 
+               {'label':'/VCategorias', 'msg':['Error al intentar crear categoría.']}, ]
     res = results[1]
     
     nombre = params.get('nombre',None)
     peso = params.get('peso',None)   
     
-    if not(( nombre == None ) and ( peso == None )): 
+    if (( nombre != None ) and ( peso != None )): 
         categoria = clsCategoria()
         creaccionCorrecta = categoria.insertar(nombre,peso)
-        
-        if( creaccionCorrecta[0]):
-            res = results[0]
-  
-    
+        res = results[0] if creaccionCorrecta[0] else results[1]
+
     if "actor" in res:
         if res['actor'] is None:
             session.pop("actor", None)
@@ -48,15 +51,19 @@ def ACrearCategoria():
             session['actor'] = res['actor']
     return json.dumps(res)
 
-
+#.-----------------------------------------------------------------------------.
 
 @cates.route('/cates/AElimCategoria')
 def AElimCategoria():
-    #GET parameter
+    
+    if 'usuario' not in session:
+      res['logout'] = '/'
+      return json.dumps(res)
+  
     idCategoria = int(request.args['idCategoria'])
-    results = [{'label':'/VCategorias', 'msg':['Categoría eliminada.']}, {'label':'/VCategorias', 'msg':['Error al intentar eliminar categoría.']}, ]
+    results = [{'label':'/VCategorias', 'msg':['Categoría eliminada.']}, 
+               {'label':'/VCategorias', 'msg':['Error al intentar eliminar categoría.']}, ]
     res = results[1]
-    #Action code goes here, res should be a list with a label and a message
 
     res['label'] = res['label'] + '/' + str(idCategoria)
     #Action code ends here
@@ -67,18 +74,30 @@ def AElimCategoria():
             session['actor'] = res['actor']
     return json.dumps(res)
 
-
+#.-----------------------------------------------------------------------------.
 
 @cates.route('/cates/AModifCategoria', methods=['POST'])
 def AModifCategoria():
-    #POST/PUT parameters
+  
+    if 'usuario' not in session:
+      res['logout'] = '/'
+      return json.dumps(res)
+  
     params = request.get_json()
-    results = [{'label':'/VCategorias', 'msg':['Categoría actualizada.']}, {'label':'/VCategorias', 'msg':['Error al intentar modificar categoría.']}, ]
+    results = [{'label':'/VCategorias', 'msg':['Categoría actualizada.']}, 
+               {'label':'/VCategorias', 'msg':['Error al intentar modificar categoría.']}, ]
+    # Resultado de la modificación de la categoría.
     res = results[1]
-    #Action code goes here, res should be a list with a label and a message
-    res['label'] = res['label'] + '/1'
+    
+    identificador = int(session['idCategoria'])
+    nombre = params.get('nombre', None)
+    peso = params.get('peso', None)
+    
+    if ((nombre != None) and (peso != None )):
+        categoria = clsCategoria()
+        modificacionCorrecta = categoria.modificar(identificador, nombre, peso)    
+        res = results[0] if modificacionCorrecta else results[1]
 
-    #Action code ends here
     if "actor" in res:
         if res['actor'] is None:
             session.pop("actor", None)
@@ -86,30 +105,31 @@ def AModifCategoria():
             session['actor'] = res['actor']
     return json.dumps(res)
 
-
+#.-----------------------------------------------------------------------------.
 
 @cates.route('/cates/VCategoria')
 def VCategoria():
-    #GET parameter
-    idCategoria = request.args['idCategoria']
+
     res = {}
     if "actor" in session:
-        res['actor']=session['actor']
-    #Action code goes here, res should be a JSON structure
+        res['actor'] = session['actor']
 
     if 'usuario' not in session:
       res['logout'] = '/'
       return json.dumps(res)
+  
+    idActual = int(request.args.get('idCategoria',1))
+    infoActual = Categorias.query.filter(Categorias.identificador == idActual).first()
     res['usuario'] = session['usuario']
-    res['idCategoria'] = int(idCategoria)
-    res['fCategoria'] = {'idCategoria':1, 'peso':3, 
-                         'nombre':'Reparacíon edl motor'}
-
-
-    #Action code ends here
+    
+    res['idCategoria'] = int(idActual)
+    session['idCategoria'] = int(idActual)
+    
+    res['fCategoria'] = {'idCategoria':idActual, 'peso':infoActual.peso, 
+                         'nombre':infoActual.nombre}
     return json.dumps(res)
 
-
+#.-----------------------------------------------------------------------------.
 
 @cates.route('/cates/VCategorias')
 def VCategorias():
@@ -121,22 +141,27 @@ def VCategorias():
       res['logout'] = '/'
       return json.dumps(res)
   
+    # Se muestra la lista de categorias.
+    categorias = Categorias.query.all()
+    
+    if (categorias != None):
+        categoria = clsCategoria()
+        categoria.insertar('Implementar una acción',2)
+        categoria.insertar('Implementar una vista',2)
+        categoria.insertar('Implementar una regla de negocio o un método de una clase',2)
+        categoria.insertar('Migrar la base de datos',2)
+        categoria.insertar('Crear un diagrama UML',1)
+        categoria.insertar('Crear datos iniciales',1)
+        categoria.insertar('Crear una prueba de aceptación',1)
+        categoria.insertar('Actualizar un elemento implementado en otra tarea',2)
+        categoria.insertar('Actualizar en elemento implementado en otra tarea',1)
+        categoria.insertar('Escribir el manual en línea de unapágina',1)
         
-    categorias = db.session.query(Categorias).all()
     res['usuario'] = session['usuario']
     res['data0'] = [
       {'idCategoria':cat.identificador, 'peso':cat.peso, 'nombre':cat.nombre }
       for cat in categorias]
-
-    #Action code ends here
+    
     return json.dumps(res)
 
-
-
-
-
-#Use case code starts here
-
-
-#Use case code ends here
-
+#.-----------------------------------------------------------------------------.
